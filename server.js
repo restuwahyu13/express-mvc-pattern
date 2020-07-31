f3228a7e0ba889b12a1e5cee72434b2bf1465fdf
restuwahyu13 <restuwahyu13@gmail.com>
2020-08-01 01:06:24 +0700

    update files

--- server.js
+++ server.js
@@ -1,58 +1,21 @@
-const cluster = require('cluster');
-const os = require('os');
-const http = require('http');
-const app = require('./app');
+const cluster = require('cluster')
+const os = require('os')
+const http = require('http')
+const app = require('./app')
 
 if (cluster.isMaster) {
+  let cpuCore = os.cpus().length
+  for (let i = 0; i < cpuCore; i++) {
+    cluster.fork()
+  }
+  cluster.on('online', (worker) => {
+    if (worker.isConnected()) console.log(`worker is active ${worker.process.pid}`)
+  })
 
-    let cpuCore = os.cpus().length;
-
-    for (let i = 0; i < cpuCore; i++) {
-
-        cluster.fork();
-    }
-
-    let workerId;
-    let workerCount = [];
-
-    for (workerId in cluster.workers) {
-
-        workerCount.push(workerId);
-    }
-    workerCount.forEach(async workerId => {
-
-        await cluster.workers[workerId].send({
-
-            from: 'master',
-            text: 'SIGKILL'
-        });
-    });
-    cluster.on('online', worker => {
-
-        if (worker.isConnected()) console.log(`worker is active ${worker.process.pid}`);
-
-    });
-    cluster.on('exit', worker => {
-
-        if (worker.isDead()) console.log(`worker is dead ${worker.process.pid}`);
-
-        cluster.fork();
-    });
-
-
-} else {
-
-    if (cluster.isWorker) {
-        process.on('message', msg => {
-
-            if (msg.type == 'SIGKILL') {
-
-                process.exit(0);
-
-            } else {
-
-                http.createServer(app).listen(process.env.PORT);
-            }
-        });
-    }
+  cluster.on('exit', (worker) => {
+    if (worker.isDead()) console.log(`worker is dead ${worker.process.pid}`)
+    cluster.fork()
+  })
 }
+
+http.createServer(app).listen(process.env.PORT)
